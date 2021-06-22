db734234e2c4d251367e13662782f919c1e6efc2
unknown <mfjkd7795@gmail.com>
2021-06-22 13:07:08 -0700

    Snake ladder 1 player game start from zero

--- src/SnakeandLadder/snakeladder.java
+++ src/SnakeandLadder/snakeladder.java
@@ -2,8 +2,11 @@ package SnakeandLadder;
 
 public class snakeladder {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Snake and Ladder game");
-	}
+public static void main(String[] args)
+{
 
+	int Position=0;
+	System.out.println("Welcome to Snake-Ladder Program Problem");
+	System.out.println("You are at Starting Position::"+Position);
+}
 }
