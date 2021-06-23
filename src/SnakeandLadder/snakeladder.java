ac0452e0cd734b00f498d91324af1403665d49dd
unknown <mfjkd7795@gmail.com>
2021-06-23 11:53:32 -0700

    Player moves forward and backwards based on ladder and snake case or else it stays at same position

--- src/SnakeandLadder/snakeladder.java
+++ src/SnakeandLadder/snakeladder.java
@@ -1,12 +1,36 @@
 package SnakeandLadder;
 
-public class snakeladder {
+public class snakeladder 
+{
 
 public static void main(String[] args)
 {
 
-	int Position=0;
-	System.out.println("Welcome to Snake-Ladder Program Problem");
-	System.out.println("You are at Starting Position::"+Position);
+	int position = 0;
+	System.out.println("position is " + position);
+	int die=(int) (Math.random() * 10) % 6+1;
+		System.out.println("dice value is " + die);
+	int value=(int) (Math.random() * 10) % 3+1;
+	System.out.println("case number is " + value);
+	 switch(value) {
+	   case 1:
+	   System.out.println( "No Play You are in the same position." );
+	   break;
+	  
+	   case 2:
+	   position = (position + die);
+	   System.out.println( "your position after ladder is ::" + position );
+	   break;
+	   
+	   case 3: 
+	   position = (position - die);
+	  
+	  if(position<0){
+		  position = 0;
+	  }
+		 System.out.println( "Your Position after snake is::" + position );
+	   break;
+	   }
+	 }
 }
-}
+
